6dd5831bc41351288d9d296aa4f4fd4011d73a82
Stefan <stefan@izdrail.com>
2025-03-06 07:29:14 +0000

    Added hotjar tracking

--- application-domain/php/resources/views/layouts/guest.blade.php
+++ application-domain/php/resources/views/layouts/guest.blade.php
@@ -6,7 +6,17 @@
         <meta name="csrf-token" content="{{ csrf_token() }}" />
 
         <title>{{ config("app.name", "Public sos - Emergency Management Online Platform") }}</title>
-
+        <!-- Hotjar Tracking Code for Public Sos -->
+        <script>
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:5327959,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        </script>
         <!-- Scripts -->
         @vite(["resources/css/app-frontend.css"])
         @vite(["resources/js/app-frontend.js"])
